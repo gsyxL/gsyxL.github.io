120237021e7c40cb9a1d7bf848671568352f70a0
lb <502787169@.qq.com>
2017-06-23 17:18:27 +0800

    d8

--- designertest/js/designertest.js
+++ designertest/js/designertest.js
@@ -1,11 +1,11 @@
 
-	$('.indexPage').click(function () {
+	$('.indexPage').on('click',function () {
 		$(this).hide()
 		$('.testBegin').show()
 	})
 	var num = 1,flag = false,restNum = -1;
 	var qLis = $('.question ul li');
-	$('.nextBtn').click(function () {
+	$('.nextBtn').on('click',function () {
 		$('#q'+num).show().siblings().hide()
 	})
 	$('.question ul li').on('click',function () {
@@ -25,7 +25,6 @@
 		var imTel = $.trim($('.im_tel').val())
 		if(restNum === -1) return
 		if(!/^[\u4e00-\u9fa5]{2,4}$/.test(imName)) {
-			console.log(1)
 			$('.tips').show().find('.tip').html('请输入正确的姓名')
 			setTimeout(function () {
 				$('.tips').hide()
@@ -34,7 +33,6 @@
 			return
 		}
 		if(!/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(imTel)) {
-				console.log(2)
 			$('.tips').show().find('.tip').html('请输入正确的号码')
 			setTimeout(function () {
 				$('.tips').hide()
