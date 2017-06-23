562b297279e8b8630b0f213347dfa0709d80b045
lb <502787169@.qq.com>
2017-06-23 16:35:06 +0800

    d

---
+++ designertest/js/designertest.js
@@ -0,0 +1,58 @@
+
+	$('.indexPage a').click(function () {
+		$('.testBegin').show();
+	})
+	var num = 1,flag = false,restNum = -1;
+	var qLis = $('.question ul li');
+	$('.nextBtn').click(function () {
+		$('#q'+num).show().siblings().hide()
+	})
+	$('.question ul li').on('click',function () {
+		$(this).addClass('lichoose').siblings().removeClass('lichoose')
+		var qsIndex = $(this).closest('.question').index(); // question索引
+		var qsLisIndex = $(this).index(); // li索引
+		//第一题和第三题
+		if(qsIndex === 0 || qsIndex === 2) { 
+			//往下跳一题
+			num = qsIndex+2;
+			return;
+		}
+	})
+	// 查看结果
+	$('.viewBtn').on('click',function () {
+		var imName = $.trim($('.im_name').val())
+		var imTel = $.trim($('.im_tel').val())
+		if(restNum === -1) return
+		if(!/^[\u4e00-\u9fa5]{2,4}$/.test(imName)) {
+			console.log(1)
+			$('.tips').show().find('.tip').html('请输入正确的姓名')
+			setTimeout(function () {
+				$('.tips').hide()
+			},3000)
+			
+			return
+		}
+		if(!/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(imTel)) {
+				console.log(2)
+			$('.tips').show().find('.tip').html('请输入正确的号码')
+			setTimeout(function () {
+				$('.tips').hide()
+			},3000)
+			return
+		}
+		$('.import').hide()
+		$('.punDesigner').eq(restNum).show().siblings().hide();
+	})
+	$('.im_tel').on('keyup',function () {
+		if($(this).val().length >= 11) {
+			$(this).attr('disabled','disabled')
+		}
+	})
+	function jumpPage(pageNum) {
+		num = pageNum
+	}
+	function resultPage(resNum) {
+		restNum = resNum
+		$('.testBegin').hide()
+		$('.import').show()
+	}	
