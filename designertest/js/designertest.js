d9c719dd5c24b067be89d81c1dfdbbc2af27e207
lb <502787169@.qq.com>
2017-06-26 10:51:09 +0800

    dd

--- designertest/js/designertest.js
+++ designertest/js/designertest.js
@@ -25,39 +25,42 @@
 		var imName = $.trim($('.im_name').val())
 		var imTel = $.trim($('.im_tel').val())
 		if(restNum === -1) return
-		if(!/^[\u4e00-\u9fa5]{2,4}$/.test(imName)) {
-			$('.tips').show().find('.tip').html('请输入正确的姓名')
-			setTimeout(function () {
-				$('.tips').hide()
-			},2000)
-			return
-		}
-		if(!/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(imTel)) {
-			$('.tips').show().find('.tip').html('请输入正确的号码')
-			setTimeout(function () {
-				$('.tips').hide()
-			},2000)
-			return
-		}
-		$.ajax({
-			url :'http://game.shjtu.cn/tools/submit_ajax.ashx?action=free_add',
-			data: { "txtname": imName, "txtmobile": imTel},
-			type: 'post',
-			dataType: 'json',
-			success: function (data) {
-				if(data.status !== 1) {
-					// $('.tips').show().find('.tip').html(data.msg)
-					// setTimeout(function () {
-					// 	$('.tips').hide()
-					// },2000)
-					return
-				}
-				$('.import').hide()
-				$('.testResult').show()
-				$('.punDesigner').eq(restNum).show().siblings(':not(.againBtn)').hide();
+			if(!/^[\u4e00-\u9fa5]{2,4}$/.test(imName)) {
+				$('.tips').show().find('.tip').html('请输入正确的姓名')
+				setTimeout(function () {
+					$('.tips').hide()
+				},2000)
+				return
 			}
+			if(!/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(imTel)) {
+				$('.tips').show().find('.tip').html('请输入正确的号码')
+				setTimeout(function () {
+					$('.tips').hide()
+				},2000)
+				return
+			}
+			// $.ajax({
+			// 	url :'http://game.shjtu.cn/tools/submit_ajax.ashx?action=free_add',
+			// 	data: { "txtname": imName, "txtmobile": imTel},
+			// 	type: 'post',
+			// 	dataType: 'json',
+			// 	success: function (data) {
+			// 	// if(data.status !== 1) {
+			// 		// $('.tips').show().find('.tip').html(data.msg)
+			// 		// setTimeout(function () {
+			// 		// 	$('.tips').hide()
+			// 		// },2000)
+			// 		// return
+			// 	// }
+			// 	// $('.import').hide()
+			// 	// $('.testResult').show()
+			// 	// $('.punDesigner').eq(restNum).show().siblings(':not(.againBtn)').hide();
+			// 	}
+			// })
+			$('.import').hide()
+			$('.testResult').show()
+			$('.punDesigner').eq(restNum).show().siblings(':not(.againBtn)').hide()
 		})
-	})
 	$('.im_tel').on('keyup',function () {
 		if($(this).val().length >= 11) {
 			$(this).val($(this).val().slice(0,11)) 
